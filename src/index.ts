1630cc65349ea1f18ebb340688183beefe304b09
dev-xero <elo48858@gmail.com>
2024-03-24 11:58:02 +0100

    feat: Listen for requests on default endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log('Index')
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config() // Load environment variables into the process
+const port = process.env.PORT || '8080'
+
+const application = express()
+
+application.get('/', (_, res) => {
+    res.json({
+        message: 'Welcome to Molla\'s backend API',
+        success: true,
+        payload: null,
+    })
+})
+
+application.use((_, res) => {
+    res.json({
+        message: 'Undefined endpoint accessed.',
+        success: false,
+        payload: null,
+    })
+})
+
+application.listen(port, () => {
+    console.log('[LOG]: application listening on port:', port)
+    console.log('[LOG]: api live on -', `http://localhost:${port}`)
+})
