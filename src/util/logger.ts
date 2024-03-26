3f2594cac49d84e08e93b13be43797358869ef44
dev-xero <elo48858@gmail.com>
2024-03-26 04:20:29 +0100

    feat: Add color highlighting depending on log level

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,8 +1,24 @@
+import colors from 'colors/safe'
+
 export enum LogLevels {
-    INFO = "INFO",
-    ERROR = "ERR"
+    INFO = 'INFO',
+    ERROR = 'ERR',
 }
 
 export const logMessage = (level: LogLevels, msg: string) => {
-    console.log(`[${level}]: ${msg}`)
+    let coloredTag
+    switch (level) {
+        case LogLevels.INFO:
+            coloredTag = colors.cyan(`[${level}]:`)
+            break
+
+        case LogLevels.ERROR:
+            coloredTag = colors.red(`[${level}]:`)
+            break
+
+        default:
+            coloredTag = colors.grey(`[${level}]:`)
+            break
+    }
+    console.log(coloredTag, msg)
 }
