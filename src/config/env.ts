89887249a626c1210fc74653c858182c2672ab08
dev-xero <elo48858@gmail.com>
2024-03-29 02:20:25 +0100

    patch: Update key path

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,7 +6,8 @@ const env = {
     port: process.env.PORT || '8080',
     environment: process.env.ENVIRONMENT || 'development',
     mongodb_uri: process.env.MONGODB_URI || '',
-    google_bucket_url: process.env.GBUCKET_URL || ''
+    google_bucket_url: process.env.GBUCKET_URL || '',
+    key_path: process.env.KEY_PAH || 'key.json'
 }
 
 export default env
--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,7 +3,7 @@ import env from './env'
 import { LogLevel, logMessage } from '@util/logger'
 
 admin.initializeApp({
-    credential: admin.credential.cert('/key.json'),
+    credential: admin.credential.cert(env.key_path),
     storageBucket: env.google_bucket_url,
 })
 
