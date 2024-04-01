bf2ec0f13199ab1223af7f475df4c3736cbccb99
dev-xero <elo48858@gmail.com>
2024-04-01 19:09:04 +0100

    docs: Add comments to sign-up request handler

--- src/route/auth.route.ts
+++ src/route/auth.route.ts
@@ -3,6 +3,15 @@ import express, { Request, Response } from 'express'
 
 const authRouter = express.Router()
 
+/*
+*   This function handles signing-up users, it accepts three parameters from the
+    request body:
+*       1. username
+*       2. password
+*       3. email
+*   It verifies the data then creates a new document containing the user data, then responds 
+*   with a JWT token and the user object.
+ */
 authRouter.post('/sign-up', (_: Request, res: Response) => {
     sendJsonResponse(
         {
