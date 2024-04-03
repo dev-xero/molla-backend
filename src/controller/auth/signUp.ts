9985c219e3e47cb7ad9a9c89ff056d1ab3b7d505
dev-xero <elo48858@gmail.com>
2024-04-03 21:42:44 +0100

    feat: Respond with custom sign-up error messages

--- src/controller/auth/signUp.ts
+++ src/controller/auth/signUp.ts
@@ -23,10 +23,11 @@ export async function signUp(req: Request, res: Response) {
 
         // Make sure the client sends the required sign-up data
         if (!isReqBodyValid.success) {
-            console.log(isReqBodyValid.error)
+            const errMessage = isReqBodyValid.error.issues[0].message
+            logMessage(LogLevel.ERROR, errMessage)
             sendJsonResponse(
                 {
-                    message: 'Invalid sign-up parameters sent.',
+                    message: errMessage,
                     code: 401,
                     payload: null,
                 },
@@ -52,61 +53,58 @@ export async function signUp(req: Request, res: Response) {
                 res,
             )
             return
-        } else {
-            const hashedPassword = hashSync(
-                userReqBody.password,
-                genSaltSync(10),
-            )
-            const dbUser: UserInterface = {
-                username: userReqBody.username,
-                password: hashedPassword,
-                email: userReqBody.email,
-                isAdmin: userReqBody.isAdmin,
-            }
-
-            // Create the new user collection and sign the jwt
-            await userModel.create(dbUser).then((user) => {
-                const signature = env.jwt_signature
-                if (signature == '') {
-                    // Configuration error
-                    userModel.findByIdAndDelete(user._id).then(() => {
-                        sendJsonResponse(
-                            {
-                                message: 'Internal server error, missing server configuration.',
-                                code: 500,
-                                payload: null,
-                            },
-                            res,
-                        )
-                    })
-                } else {
-                    jwt.sign(
-                        { id: user.id },
-                        signature,
-                        (_: unknown, token: any) => {
-                            logMessage(
-                                LogLevel.SUCCESS,
-                                'successfully created new user.',
-                            )
-                            logMessage(LogLevel.INFO, user)
-
-                            sendJsonResponse(
-                                {
-                                    message: 'Successfully created new user.',
-                                    code: 201,
-                                    payload: {
-                                        user,
-                                        token,
-                                    },
-                                },
-                                res,
-                            )
-                            return
-                        },
-                    )
-                }
-            })
         }
+
+        // Don't save the plain text password, hash it first
+        const hashedPassword = hashSync(userReqBody.password, genSaltSync(10))
+        const dbUser: UserInterface = {
+            username: userReqBody.username,
+            password: hashedPassword,
+            email: userReqBody.email,
+            isAdmin: userReqBody.isAdmin,
+        }
+
+        // Create the new user collection and sign the jwt
+        await userModel.create(dbUser).then((user) => {
+            const signature = env.jwt_signature
+            if (signature == '') {
+                // Configuration error
+                userModel.findByIdAndDelete(user._id).then(() => {
+                    sendJsonResponse(
+                        {
+                            message:
+                                'Internal server error, missing server configuration.',
+                            code: 500,
+                            payload: null,
+                        },
+                        res,
+                    )
+                })
+            } else {
+                // User passed all checks, successfully created
+                const cb = (_: unknown, token: any) => {
+                    logMessage(
+                        LogLevel.SUCCESS,
+                        'successfully created new user.',
+                    )
+                    logMessage(LogLevel.INFO, user)
+
+                    sendJsonResponse(
+                        {
+                            message: 'Successfully created new user.',
+                            code: 201,
+                            payload: {
+                                user,
+                                token,
+                            },
+                        },
+                        res,
+                    )
+                    return
+                }
+                jwt.sign({ id: user.id }, signature, cb)
+            }
+        })
     } catch (error) {
         logMessage(LogLevel.ERROR, 'failed to sign-up user.')
         logMessage(LogLevel.ERROR, error)
--- src/validator/user.validator.ts
+++ src/validator/user.validator.ts
@@ -1,8 +1,14 @@
 import { z } from 'zod'
 
 export const userZodSchema = z.object({
-    username: z.string().min(4).max(24),
-    password: z.string().min(4).max(24),
-    email: z.string().email(),
+    username: z
+        .string()
+        .min(4, { message: 'Username must be at least 4 characters long.' })
+        .max(24, { message: 'Username must not exceed 24 characters.' }),
+    password: z
+        .string()
+        .min(4, { message: 'Min password length is 4 characters.' })
+        .max(24, { message: 'Max password length is 24 characters.' }),
+    email: z.string().email({ message: 'Provide a valid email address.' }),
     isAdmin: z.enum(['true', 'false']).transform((value) => value === 'true'), // weirdly enough zod strings don't read "false" as false
 })
